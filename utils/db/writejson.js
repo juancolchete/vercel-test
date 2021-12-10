70af65e2a126ea90c77cb37e05687127c450d043
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-12-10 14:59:08 -0300

    Add

--- utils/db/writejson.js
+++ utils/db/writejson.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 console.log(process.env.JSON_CONFIG);
 const customer = process.env.JSON_CONFIG;
 const jsonString = JSON.stringify(customer)
-fs.writeFile('./serviceAccountKey.json', jsonString, err => {
+fs.writeFile('./utils/db/serviceAccountKey.json', jsonString, err => {
     if (err) {
         console.log('Error writing file', err)
     } else {
